2840ee151672d27881b37d4b60a486b2e9871a54
ibrahimoguzhany <ibrahimoguzhany@gmail.com>
2021-07-04 01:43:23 +0300

    init

---
+++ pages/portfolios/[id].js
@@ -0,0 +1,27 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+
+const PortfolioDetail = () => {
+  const router = useRouter();
+  const id = router.query.id;
+  return <h1>I am Details Page with ID: {id}</h1>;
+};
+
+PortfolioDetail.getInitialProps = ({ query }) => {
+  return { query };
+};
+
+export default PortfolioDetail;
+
+// class PortfolioDetail extends React.Component {
+//   static getInitialProps({ query }) {
+//     return { query };
+//   }
+
+//   render() {
+//     const id = this.props.query.id;
+//     return <h1>I am Detail Page with id : {id} </h1>;
+//   }
+// }
+
+// export default PortfolioDetail;
